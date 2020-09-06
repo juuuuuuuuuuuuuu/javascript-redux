e47299883dd90a4bf81374ba5200ddec8eae7413
이주영 <juu0124@wemakeprice.com>
2020-09-06 16:55:08 +0900

    (master): dispatch 개념

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import createStore from './redux';
 
 
-// state의 어떤 값을 바꿀껀지 넘겨줌
 function reducer(state, action) {
   if (action.type === 'abc') {
     // 객체 참조형이서 직접 바꾸면 안됨 (새로운 객체로 만들어서 리턴)
@@ -14,5 +13,6 @@ function reducer(state, action) {
 
 const store = createStore(reducer);
 
+dispatchEvent({ type: 'abc' });
 
-
+console.log(store.getState());
--- redux.js
+++ redux.js
@@ -4,13 +4,15 @@ export function createStore(reducer) {
   // 상태값 밖에서 못바꾸게 closure 사용하여 은닉화
   const getState = () => ({ ...state });
 
-  // 상태가 바뀔때 reducer호출하면 현재 상태와 action을 넘긴다.
-  // 값을 리턴해주면 state로 업데이트
-  state = reducer(state, {
-    type: 'abc',
-  });
+  // 컴포넌트가 어떤 액션을 하라고 던지는 행위
+  // dispatch: 액션을 받아서 리듀서를 호출해준다.
+  // 리듀서의 반환받은 값을 업데이트
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  }
 
   return {
     getState,
+    dispatch,
   };
 }
